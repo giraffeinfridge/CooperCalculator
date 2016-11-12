8a958eb59de90006399ad7fb8ee5f8d5500f3655
Amir <Amir@LAPTOP-9MJJKFIU>
2016-11-12 11:41:31 -0500

    Finished BasicCalculator.java cuz im a f*cking beast

--- CalculatorSciFair/src/BasicCalculator.java
+++ CalculatorSciFair/src/BasicCalculator.java
@@ -1,58 +1,23 @@
 import java.util.Scanner;
 
-public class BasicCalculator {
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
-	public static int operation; // 1 = add, 2 = subtract,
-									// 3 = multiply, 4 = divide
+public class BasicCalculator {
 
 
 	public static void main(String[] args) {
 		Scanner bc = new Scanner(System.in);
-		String input;
-
-		input = bc.nextLine();
-
-		char[] inputChars = input.toCharArray();
-		for (char c : inputChars) {
-			if (isnum(c)) {
-				System.out.println("Number");
-			} else if (isop(c)) {
-				System.out.println("Operation");
-			} else {
-				System.out.println("[-] Error: " + c + "is not a number or an operation");
-			}
+		String input = bc.nextLine();
+		ScriptEngineManager mgr = new ScriptEngineManager();
+	    ScriptEngine engine = mgr.getEngineByName("JavaScript");
+	    try {
+			System.out.println(engine.eval(input));
+		} catch (ScriptException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
-	public static boolean isnum(char c) {
-		if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8'
-				|| c == '9') {
-			return true;
-		} else {
-			return false;
-		}
-	}
-
-	public static boolean isop(char c) {
-		if (c == '+' || c == '-' || c == '*' || c == '/') {
-			return true;
-		} else {
-			return false;
-		}
-
-	}
-	public static int getop(char c){
-		switch(c){
-		case '+':
-			return 1;
-		case '-':
-			return 2;
-		case '*':
-			return 3;
-		case '/':
-			return 4;
-			default:
-				return 0;
-		}
-	}
 }
