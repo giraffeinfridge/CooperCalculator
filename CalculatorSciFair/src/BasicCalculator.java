c59899af0c543fd13072223aabdf111b0ed38c7d
Amir <Amir@LAPTOP-9MJJKFIU>
2016-11-11 23:29:25 -0500

    iterates through input and determines which chars are numbers, and ops

--- CalculatorSciFair/src/BasicCalculator.java
+++ CalculatorSciFair/src/BasicCalculator.java
@@ -1,41 +1,58 @@
 import java.util.Scanner;
+
 public class BasicCalculator {
-	
-	public static int operation; //1 = add, 2 = subtract, 3 = multiply, 4 = divide
-	public static int opindex;
-	public static int firstnum;
-	public static int secondnum;
-	
+
+	public static int operation; // 1 = add, 2 = subtract,
+									// 3 = multiply, 4 = divide
+
+
 	public static void main(String[] args) {
-		Scanner bc = new Scanner (System.in);
+		Scanner bc = new Scanner(System.in);
 		String input;
-		
+
 		input = bc.nextLine();
-		
+
 		char[] inputChars = input.toCharArray();
-		
-		for(int i = 0; i < inputChars.length; i++){
-			//Check if each char is number or operation
-			//Sample equation: number operator number 69-12
-			if(inputChars[i] == '+'){
-				operation = 1;
-				opindex = i;
-			}else if(inputChars[i] == '-'){
-				operation = 2;
-				opindex = i;
-			}else if(inputChars[i] == '*'){
-				operation = 3;
-				opindex = i;
-			}else if(inputChars[i] == '/'){
-				operation = 4;
-				opindex = i;
+		for (char c : inputChars) {
+			if (isnum(c)) {
+				System.out.println("Number");
+			} else if (isop(c)) {
+				System.out.println("Operation");
+			} else {
+				System.out.println("[-] Error: " + c + "is not a number or an operation");
 			}
-			
-			
+		}
+	}
+
+	public static boolean isnum(char c) {
+		if (c == '0' || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7' || c == '8'
+				|| c == '9') {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public static boolean isop(char c) {
+		if (c == '+' || c == '-' || c == '*' || c == '/') {
+			return true;
+		} else {
+			return false;
 		}
 
 	}
-	
-	//TODO: assign all numbers before operation to firstnum, and all after secondnum
-	
+	public static int getop(char c){
+		switch(c){
+		case '+':
+			return 1;
+		case '-':
+			return 2;
+		case '*':
+			return 3;
+		case '/':
+			return 4;
+			default:
+				return 0;
+		}
+	}
 }
