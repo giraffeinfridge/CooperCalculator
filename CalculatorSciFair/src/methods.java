93848559e5b39fec1c220f9a632df358be0872a0
Amir <Amir@LAPTOP-9MJJKFIU>
2016-11-13 13:53:38 -0500

    methods class for reference

--- CalculatorSciFair/src/methods.java
+++ CalculatorSciFair/src/methods.java
@@ -1,8 +1,30 @@
+import java.util.Scanner;
+
 public class methods {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter explanation number: ");
+		int ex = in.nextInt();
+		System.out.println(explanation(ex));
+		thanks();
 	}
-
+	
+	public static String explanation(int ex){
+		switch(ex){
+			case 0:
+				return "No Explanation";
+			case 1:
+				return "First explanation";
+			case 2:
+				return "second explanation";
+			default:
+				return "enter a number";
+		}
+	}
+	public static void thanks(){
+		System.out.println("thanks for using my calculator");
+	}
+	
 }
