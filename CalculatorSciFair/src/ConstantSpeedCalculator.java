b850591456edd0cb13e6cb5d04e103300ee91258
Amir <Amir@LAPTOP-9MJJKFIU>
2016-11-13 20:35:02 -0500

    added constant speed calculator to main menu

--- CalculatorSciFair/src/ConstantSpeedCalculator.java
+++ CalculatorSciFair/src/ConstantSpeedCalculator.java
@@ -1,81 +1,85 @@
 import java.util.Scanner;
+
 public class ConstantSpeedCalculator {
 
-	public static void main(String[] args) {
-		Scanner cspeed = new Scanner (System.in);
-		
-		System.out.println("Speed (m/s):");
-		
-		float spd = cspeed.nextFloat();
-		
-		System.out.println("Time (s):");
-		
-		float time = cspeed.nextFloat();
-		
-		float answer = time * spd;
-		
-		System.out.println();
-		
-		System.out.println(answer + "m");
-		
-		try {
-			Thread.sleep(1000);
-		}	catch(InterruptedException balls) {
-			Thread.currentThread().interrupt();
+	public static void init() {
+		Scanner cspeed = new Scanner(System.in);
+		boolean on = true;
+		while (on) {
+			System.out.println("Speed (m/s):");
+
+			float spd = cspeed.nextFloat();
+
+			System.out.println("Time (s):");
+
+			float time = cspeed.nextFloat();
+
+			float answer = time * spd;
+
+			System.out.println();
+
+			System.out.println(answer + "m");
+
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException balls) {
+				Thread.currentThread().interrupt();
+			}
+
+			System.out.println();
+
+			System.out.println("Enter '1' for help or '0' to go back");
+
+			int helper = cspeed.nextInt();
+
+			System.out.println();
+
+			if (helper == 1) {
+				System.out.println("To calculate force, the formula is D = st");
+
+				System.out.println();
+
+				try {
+					Thread.sleep(2000);
+				} catch (InterruptedException balls) {
+					Thread.currentThread().interrupt();
+				}
+
+				System.out.println("'D' is distance, 's' is speed, and 't' is time");
+
+				System.out.println();
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException balls) {
+					Thread.currentThread().interrupt();
+				}
+
+				System.out.println("D = st");
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException balls) {
+					Thread.currentThread().interrupt();
+				}
+
+				System.out.println("D = " + spd + " * " + time);
+
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException balls) {
+					Thread.currentThread().interrupt();
+				}
+
+				System.out.println("D = " + answer);
+
+			}else if(helper == 0){
+				on = false;
+				break;
+			}
+
 		}
-		
-		System.out.println();
-		
-		System.out.println("Enter '1' for help");
-		
-		int helper = cspeed.nextInt();
-		
-		System.out.println();
-		
-		if (helper == 1) {
-			System.out.println("To calculate force, the formula is D = st");
-			
-			System.out.println();
-			
-			try {
-				Thread.sleep(2000);
-			}	catch(InterruptedException balls) {
-				Thread.currentThread().interrupt();
-			}
-			
-			System.out.println("'D' is distance, 's' is speed, and 't' is time");
-			
-			System.out.println();
-			
-			try {
-				Thread.sleep(1000);
-			}	catch(InterruptedException balls) {
-				Thread.currentThread().interrupt();
-			}
-			
-			System.out.println("D = st");
-			
-			try {
-				Thread.sleep(1000);
-			}	catch(InterruptedException balls) {
-				Thread.currentThread().interrupt();
-			}
-			
-			System.out.println("D = " + spd + " * " + time);
-			
-			try {
-				Thread.sleep(1000);
-			}	catch(InterruptedException balls) {
-				Thread.currentThread().interrupt();
-			}
-			
-			System.out.println("D = " + answer);
-			
-			
-		
 
 	}
 
-}
-	
 }
--- CalculatorSciFair/src/MainMenu.java
+++ CalculatorSciFair/src/MainMenu.java
@@ -2,23 +2,24 @@ import java.util.Scanner;
 
 public class MainMenu {
 
-	public static ImpulseCalculator ic = new ImpulseCalculator();
-
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		boolean on = true;
 		while (on) {
 			System.out.println("What would you like to calculate? ");
-			System.out.println("\n[0]  Quit\nPhysics\n[1]  Impulse\nMath\n[2] Basic Expression ");
+			System.out.println("\n[0]  Quit\nPhysics\n[1]  Impulse\n[2]  Constant Speed\nMath\n[3] Basic Expression ");
 			int input = in.nextInt();
 			switch (input) {
 			case 0:
 				on = false;
 				return;
 			case 1:
-				ic.init();
+				ImpulseCalculator.init();
 				break;
 			case 2:
+				ConstantSpeedCalculator.init();
+				break;
+			case 3:
 				BasicCalculator.init();
 				break;
 			}
